068f7e41663c7e4afcad014a44a211a0730276c8
CHEN patrick <chenp@informatique.univ-paris-diderot.fr>
2016-06-07 16:32:14 +0200

    ajout goumba

--- org/maker/character/GoumbaAction.java
+++ org/maker/character/GoumbaAction.java
@@ -11,6 +11,7 @@ public class GoumbaAction extends TimerTask{
   Goumba gou ;
   int count ;
   int direction ;
+  int direct=0 ;
   public GoumbaAction(Goumba gou){
       this.gou=gou ;
       direction =gou.pos[0];
@@ -23,14 +24,27 @@ public class GoumbaAction extends TimerTask{
     }else{
       this.gou.face ="Goomba_Normal_2.png";
     }
-    if((this.gou.content.grid.camera[gou.pos[1]][direction-1]).equals("N") && direction >0){
+    if((this.gou.content.grid.camera[gou.pos[1]][direction-1]).equals("N") && this.direct ==0){
       direction -- ;
       this.gou.content.grid.camera[gou.pos[1]][direction]=gou.TYPE+"";
       this.gou.content.repaint() ;
       Toolkit.getDefaultToolkit().sync();
     }else{
+      this.direct =1 ;
+    }
+    if((this.gou.content.grid.camera[gou.pos[1]][direction+1]).equals("N") && this.direct ==1){
+      direction ++ ;
+      this.gou.content.grid.camera[gou.pos[1]][direction]=gou.TYPE+"";
+      this.gou.content.repaint() ;
+      Toolkit.getDefaultToolkit().sync();
+    }else{
+      this.direct =0 ;
+    }
+
+    if(direction < 1){
       this.gou.content.grid.camera[gou.pos[1]][direction]=gou.TYPE+"";
       this.cancel() ;
     }
+
   }
 }

d40fe298803beaa983ad4907853d314cc6000eb4
CHEN patrick <chenp@informatique.univ-paris-diderot.fr>
2016-06-07 16:14:33 +0200

    ...

--- org/maker/character/GoumbaAction.java
+++ org/maker/character/GoumbaAction.java
@@ -6,16 +6,31 @@ import org.maker.character.Goumba ;
 import java.util.*;
 import java.awt.Toolkit ;
 
+
 public class GoumbaAction extends TimerTask{
   Goumba gou ;
   int count ;
+  int direction ;
   public GoumbaAction(Goumba gou){
       this.gou=gou ;
+      direction =gou.pos[0];
   }
 
   public void run(){
-    System.out.println("spider");
-    this.gou.content.grid.camera[0][0]="P";
-    this.gou.content.repaint() ;
+    this.gou.content.grid.camera[gou.pos[1]][direction]="N";
+    if(direction % 2 == 0){
+      this.gou.face ="Goomba_Normal_1.png";
+    }else{
+      this.gou.face ="Goomba_Normal_2.png";
+    }
+    if((this.gou.content.grid.camera[gou.pos[1]][direction-1]).equals("N") && direction >0){
+      direction -- ;
+      this.gou.content.grid.camera[gou.pos[1]][direction]=gou.TYPE+"";
+      this.gou.content.repaint() ;
+      Toolkit.getDefaultToolkit().sync();
+    }else{
+      this.gou.content.grid.camera[gou.pos[1]][direction]=gou.TYPE+"";
+      this.cancel() ;
+    }
   }
 }
